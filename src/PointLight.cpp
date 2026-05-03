cb55de1f5526d5a2c903b54b6d3a66ebd00189c4
duhdiamond <duhdiamond76@gmail.com>
2026-05-02 17:56:52 -0700

    Commit 8: Moved code into Camera class, started work on various Light classes, and added more elements of the scene from recent changes into the Scene class. No new videos for this commit, as most changes are related to code structure

--- src/PointLight.cpp
+++ src/PointLight.cpp
@@ -3,9 +3,6 @@
 
 void PointLight::updateLightInCurrentShader() {
     unsigned int currShaderID;
-    TexturedMonkeyShader.setVec3("pointlights[0].pos", pos);
-    TexturedMonkeyShader.setVec3("pointlights[0].colour", colour);
+    // TexturedMonkeyShader.setVec3("pointlights[0].pos", pos);
+    // TexturedMonkeyShader.setVec3("pointlights[0].colour", colour);
 }
-
-TexturedMonkeyShader.setMat4("view", activeCamera->view);
-TexturedMonkeyShader.setVec3("cameraViewPos", activeCamera->cameraPos);
